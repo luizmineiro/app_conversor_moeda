112e60a2385145280cc4c2c352e2f7d50d9c3872
João Luiz <jluizmineiro@gmail.com>
2022-11-21 14:41:36 -0300

    adicionando elementos

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -6,13 +6,35 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Image.asset('assets/currency_converter.png', width: 200,),
-          ),
-        ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/currency_converter.png',
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: DropdownButton(items: [], onChanged: (value) {}),
+                  ),
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("CONVERTER"),
+            ),
+          ],
+        ),
       ),
     );
   }
