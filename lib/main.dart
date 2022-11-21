ee5fbe9b2bbf1e9227489b920f995b7ad6154b26
João Luiz <jluizmineiro@gmail.com>
2022-11-21 07:56:58 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cripto_app/pages/moedas_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const MoedasPage(),
+    );
+  }
+}
+
---
+++ lib/pages/moedas_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MoedasPage extends StatelessWidget {
+  const MoedasPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cripto Moedas'),
+      ),
+    );
+  }
+}
