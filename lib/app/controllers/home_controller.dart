c4a3253fc2fa5d9f520f03896c7fc8ec39e439ee
luizmineiro <jluizmineiro@gmail.com>
2022-11-23 09:10:46 -0300

    finalizando controller e executando testes

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -4,27 +4,33 @@ import 'package:flutter/material.dart';
 class HomeController {
   List<CurrencyModel>? currencies;
 
-  TextEditingController toText = TextEditingController();
-  TextEditingController fromText = TextEditingController();
+  final TextEditingController? toText;
+  final TextEditingController? fromText;
 
   CurrencyModel? toCurrency;
   CurrencyModel? fromCurrency;
 
-  HomeController() {
+  HomeController({this.toText, this.fromText}) {
     currencies = CurrencyModel.getCurrencies();
     toCurrency = currencies![0];
     fromCurrency = currencies![1];
   }
 
   void convert() {
-    String text = toText.text;
+    String text = toText!.text;
     double value = double.parse(text);
     double returnValue = 0;
 
     if (fromCurrency?.name == 'Real') {
-      returnValue = value * toCurrency.real;
+      returnValue = value * toCurrency!.real!;
     } else if (fromCurrency?.name == 'Dolar') {
-
+      returnValue = value * toCurrency!.dolar!;
+    } else if (fromCurrency?.name == 'Euro') {
+      returnValue = value * toCurrency!.euro!;
+    } else if (fromCurrency?.name == 'Bitcoin') {
+      returnValue = value * toCurrency!.bitcoin!;
     }
+
+    fromText!.text = returnValue.toStringAsFixed(2);
   }
 }
