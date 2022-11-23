3a69d704c754d2e32236cd1ef24d25cce036f03e
luizmineiro <jluizmineiro@gmail.com>
2022-11-23 11:23:13 -0300

    finalizando app

--- lib/app/components/currency_box.dart
+++ lib/app/components/currency_box.dart
@@ -1,9 +1,21 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:cripto_app/app/models/currency_model.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyBox extends StatelessWidget {
-  const CurrencyBox({super.key});
+  final List<CurrencyModel> items;
+  final CurrencyModel selectedItem;
+  final TextEditingController controller;
+  final void Function(CurrencyModel? model)? onChanged;
+
+  const CurrencyBox({
+    super.key,
+    required this.items,
+    required this.controller,
+    required this.onChanged,
+    required this.selectedItem,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +25,19 @@ class CurrencyBox extends StatelessWidget {
         Expanded(
           child: SizedBox(
             height: 56,
-            child: DropdownButton(
+            child: DropdownButton<CurrencyModel>(
               isExpanded: true,
+              value: selectedItem,
               underline: Container(height: 1, color: Colors.amber),
-              items: [],
-              onChanged: (value) {},
+              items: items
+                  .map(
+                    (e) => DropdownMenuItem<CurrencyModel>(
+                      value: e,
+                      child: Text(e.name!),
+                    ),
+                  )
+                  .toList(),
+              onChanged: onChanged,
             ),
           ),
         ),
@@ -27,6 +47,7 @@ class CurrencyBox extends StatelessWidget {
         Expanded(
           flex: 2,
           child: TextField(
+            controller: controller,
             decoration: InputDecoration(
               enabledBorder: UnderlineInputBorder(
                 borderSide: BorderSide(
--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,14 +1,30 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, must_be_immutable
 
 import 'package:cripto_app/app/components/currency_box.dart';
+import 'package:cripto_app/app/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends StatefulWidget {
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final TextEditingController? toText = TextEditingController();
+
+  final TextEditingController? fromText = TextEditingController();
+  late HomeController homeController;
+
+  @override
+  void initState() {
+    super.initState();
+    homeController = HomeController(toText: toText, fromText: fromText);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -22,15 +38,35 @@ class HomeView extends StatelessWidget {
                 height: 150,
               ),
               SizedBox(height: 40),
-              CurrencyBox(),
+              CurrencyBox(
+                selectedItem: homeController.toCurrency!,
+                controller: toText!,
+                items: homeController.currencies!,
+                onChanged: (model) {
+                  setState(() {
+                    homeController.toCurrency = model;
+                  });
+                },
+              ),
               SizedBox(height: 20),
-              CurrencyBox(),
+              CurrencyBox(
+                selectedItem: homeController.fromCurrency!,
+                controller: fromText!,
+                items: homeController.currencies!,
+                onChanged: (model) {
+                  setState(() {
+                    homeController.fromCurrency = model;
+                  });
+                },
+              ),
               SizedBox(height: 50),
               ElevatedButton(
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(Colors.amber),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  homeController.convert();
+                },
                 child: const Text("CONVERTER"),
               ),
             ],
